f6801c25bcfc7f40fc7a1f87420b03cc21c5fffe
Ian T <iantevesbusinessacc@outlook.com>
2024-09-02 15:30:16 +1200

    Added more functionality and configs
    Honestly I forgot what I added. But I'm pretty sure their all just configs for neovim behaviours.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),                                 -- Use <Tab> to confirm completion
+    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), -- Rebind next item to <C-j>
+    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), -- Rebind previous item to <C-k>
+    ['<CR>'] = cmp.mapping(function(fallback)
+      fallback()                                                                        -- Prevent <Enter> from confirming the completion
+    end, { 'i', 's' }),
+  },
+})
+
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+vim.opt.whichwrap:append("<,>,h,l")
